deab663b39d9a9ef66c9e2b681c46985b849ba7f
Nathan Jeanbourquin <njeanbou@iMac-de-Nathan.local>
2024-03-12 16:32:36 +0100

    clear input

---
+++ includes/minishell.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njeanbou <njeanbou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 15:51:36 by njeanbou          #+#    #+#             */
+/*   Updated: 2024/03/12 16:12:24 by njeanbou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# include <sys/types.h>
+# include <dirent.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <errno.h>
+
+/* Constante */
+
+/* Error messages */
+
+// MINISHELL
+
+// SYNTAX
+
+// EXIT CMD
+
+// CD CMD
+
+#endif
---
+++ main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njeanbou <njeanbou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 15:51:14 by njeanbou          #+#    #+#             */
+/*   Updated: 2024/03/12 16:06:37 by njeanbou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	*get_input(void)
+{
+	char	*raw_input;
+	char	*input;
+
+	raw_input = readline("minishell$ ");
+	input = clean_input(raw_input, " \t");
+	return (input);
+}	
+
+int	main (int ac, char **av)
+{
+	
+}
